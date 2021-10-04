4e406576c460b3245303f232a7aafaded828b213
jesaspiring <jesiahrj@gmail.com>
2021-10-04 10:23:04 +0800

    Initial Commit

---
+++ frmLoadData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cane_Tracking
+{
+    public partial class frmLoadData : Form
+    {
+        public frmLoadData()
+        {
+            InitializeComponent();
+        }
+    }
+}
