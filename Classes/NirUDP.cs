14ea3caaa2c2dcc51309f9903bb925b2a8cb856e
jesaspiring <jesiahrj@gmail.com>
2021-11-03 16:19:37 +0800

    Added sending data functions

--- Classes/NirUDP.cs
+++ Classes/NirUDP.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -29,12 +30,53 @@ namespace Cane_Tracking.Classes
                     Udp_connected = true;
                 }
             }
-            catch (Exception)
+            catch (SocketException ex)
             {
+                Console.WriteLine(ex.ToString());
                 Udp_connected = false;
             }
 
             return Udp_connected;
+        }
+
+        private void UdpSendMessage(byte[] data)
+        {
+            try
+            {
+                string ipAddress = cnf.NirAddress;
+                int sendPort = cnf.NirPort;
+
+                using (var client = new UdpClient())
+                {
+                    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                    client.Connect(ep);
+                    client.Send(data, data.Length);
+                }
+            }
+            catch (SocketException)
+            {
+                
+            }
+        }
+
+        public void SendMessage(string sample)
+        {
+            string sendString = cnf.NirSendMessage;
+            string sampleToScan = String.Concat(sendString, sample);
+
+            byte[] data = Encoding.ASCII.GetBytes(sampleToScan);
+
+            UdpSendMessage(data);
+        }
+
+        public void EndMessage(string sample)
+        {
+            string sendString = cnf.NirEndMessage;
+            string sampleToScan = String.Concat(sendString, sample);
+
+            byte[] data = Encoding.ASCII.GetBytes(sampleToScan);
+
+            UdpSendMessage(data);
         }
     }
 }
