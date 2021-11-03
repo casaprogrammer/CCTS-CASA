094c7180d8478541fa813c85c78338dac6870971
jesaspiring <jesiahrj@gmail.com>
2021-11-03 16:18:10 +0800

    Initial Commit

---
+++ frmEventLogs.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Cane_Tracking
+{
+    partial class frmEventLogs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEventLogs));
+            this.dgvEventLogs = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEventLogs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEventLogs
+            // 
+            this.dgvEventLogs.AllowUserToAddRows = false;
+            this.dgvEventLogs.AllowUserToDeleteRows = false;
+            this.dgvEventLogs.AllowUserToOrderColumns = true;
+            this.dgvEventLogs.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEventLogs.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvEventLogs.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvEventLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvEventLogs.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvEventLogs.Location = new System.Drawing.Point(12, 12);
+            this.dgvEventLogs.Name = "dgvEventLogs";
+            this.dgvEventLogs.ReadOnly = true;
+            this.dgvEventLogs.RowHeadersWidth = 62;
+            this.dgvEventLogs.RowTemplate.Height = 28;
+            this.dgvEventLogs.Size = new System.Drawing.Size(1096, 489);
+            this.dgvEventLogs.TabIndex = 0;
+            // 
+            // frmEventLogs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 513);
+            this.Controls.Add(this.dgvEventLogs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEventLogs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Event Logs";
+            this.Load += new System.EventHandler(this.frmEventLogs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEventLogs)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEventLogs;
+    }
+}
---
+++ frmEventLogs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Cane_Tracking.Classes;
+
+namespace Cane_Tracking
+{
+    public partial class frmEventLogs : Form
+    {
+
+        Queries query = new Queries();
+        ConfigValues cnf = new ConfigValues();
+        SqlConnection con;
+
+
+        public frmEventLogs()
+        {
+            InitializeComponent();
+            con = new SqlConnection(cnf.DbAddress);
+        }
+
+        private void frmEventLogs_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand(query.ListEventLog(), con);
+
+            try
+            {
+                con.Open();
+
+                if (cmd.ExecuteScalar() == null)
+                {
+                    MessageBox.Show("No Records found");
+                    this.Close();
+                }
+                else
+                {
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvEventLogs.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
