aa4852b82d6d249d6a27f9524ba9817e062a0fb4
jesaspiring <jesiahrj@gmail.com>
2021-11-06 14:35:43 +0800

    Added check database connection and create table if does not exist.

--- Classes/AppLogging.cs
+++ Classes/AppLogging.cs
@@ -14,57 +14,28 @@ namespace Cane_Tracking.Classes
         Queries query = new Queries();
         ConfigValues cnf = new ConfigValues();
 
-        SqlConnection con;
 
         private static bool State_is_saved { get; set; }
+        private static bool Db_Exist { get; set; }
         private static bool App_Db_Connected { get; set; }
         private static bool Wb_Db_Connected { get; set; }
 
-        public AppLogging()
-        {
-            con = new SqlConnection(cnf.DbAddress);
-        }
+        SqlConnection con;
 
         public void AppEventLog(string log)
         {
-            SqlCommand cmd = new SqlCommand(query.SaveLog(log), con);
-
-            try
+            if (App_Db_Connected)
             {
-                con.Open();
-                cmd.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-        }
-
-        public void SavingState(TrackingList tl, string seriesNo)
-        {
-            for (int i = 0; i < tl.lTbox.Count; i++)
-            {
-                SqlCommand cmd = new SqlCommand(query.SaveStateLog
-                                                                  (tl.lTbox[i].Item1.Text,
-                                                                   tl.lTbox[i].Item2.Text,
-                                                                   tl.lTbox[i].Item3,
-                                                                   DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"),
-                                                                   seriesNo),
-                                                                   con);
+                con = new SqlConnection(cnf.DbAddress);
+                SqlCommand cmd = new SqlCommand(query.SaveLog(log), con);
 
                 try
                 {
                     con.Open();
                     cmd.ExecuteNonQuery();
-                    State_is_saved = true;
                 }
                 catch (SqlException ex)
                 {
-                    State_is_saved = false;
                     MessageBox.Show(ex.Message);
                 }
                 finally
@@ -74,12 +45,68 @@ namespace Cane_Tracking.Classes
             }
         }
 
+        public void SavingState(TrackingList tl, string seriesNo)
+        {
+            if (App_Db_Connected)
+            {
+                con = new SqlConnection(cnf.DbAddress);
+                for (int i = 0; i < tl.lTbox.Count; i++)
+                {
+                    SqlCommand cmd = new SqlCommand(query.SaveStateLog
+                                                                      (tl.lTbox[i].Item1.Text,
+                                                                       tl.lTbox[i].Item2.Text,
+                                                                       tl.lTbox[i].Item3,
+                                                                       DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"),
+                                                                       seriesNo),
+                                                                       con);
+
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        State_is_saved = true;
+                    }
+                    catch (SqlException ex)
+                    {
+                        State_is_saved = false;
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+                }
+            }
+        }
+
         public bool StateSaved()
         {
             return State_is_saved;
         }
 
-        public bool DbConnected()
+        public bool DbConnectionExist()
+        {
+            SqlConnection appCon = new SqlConnection(cnf.DefaultConnection);
+
+            try
+            {
+                appCon.Open();
+                Db_Exist = true;
+                DbConnect();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                appCon.Close();
+            }
+
+            return Db_Exist;
+        }
+
+        public bool DbConnect()
         {
             SqlConnection appCon = new SqlConnection(cnf.DbAddress);
 
@@ -88,9 +115,10 @@ namespace Cane_Tracking.Classes
                 appCon.Open();
                 App_Db_Connected = true;
             }
-            catch (SqlException ex)
+            catch (SqlException)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("canetracking table not found, App will create a table", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CreateDB();
             }
             finally
             {
@@ -102,7 +130,7 @@ namespace Cane_Tracking.Classes
 
         public bool WbDBConnected()
         {
-            SqlConnection appCon = new SqlConnection(cnf.DbAddress);
+            SqlConnection appCon = new SqlConnection(cnf.WbAdress);
 
             try
             {
@@ -138,6 +166,54 @@ namespace Cane_Tracking.Classes
             {
                 con.Close();
             }
+        }
+
+        private void CreateDB()
+        {
+            SqlConnection newCon = new SqlConnection(cnf.DefaultConnection);
+            SqlCommand cmd = new SqlCommand(query.CreateAppDB(), newCon);
+
+            try
+            {
+                newCon.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                CreateAppLogsTable();
+                newCon.Close();
+            }
+        }
+
+        private void CreateAppLogsTable()
+        {
+            SqlConnection newCon = new SqlConnection(cnf.DefaultConnection);
+            SqlCommand cmd = new SqlCommand(query.CreateTable(), newCon);
+
+            try
+            {
+                newCon.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                DbConnect();
+                newCon.Close();
+            }
+        }
+
+        public bool DbConnectionSuccess()
+        {
+            return App_Db_Connected;
         }
     }
 }
